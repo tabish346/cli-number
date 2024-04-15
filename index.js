4eedf420bd9ed878ac5f93768fe8fb3b69174237
tabish@DESKTOP <tabishludo@gmail.com>
2024-04-15 21:30:24 +0500

    created number guessing

---
+++ index.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+//Computer will generate a andom number 
+//user input for guessing number
+//compare user input with computer generated number and show result
+const randomnumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "usernumber",
+        type: "number",
+        message: "please guess a number between 1-6",
+    },
+]);
+if (answer.usernumber === randomnumber) {
+    console.log("congratulation! you guessed right number.");
+}
+else {
+    console.log("you guess wrong number");
+}
